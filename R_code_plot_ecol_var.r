8ca713df780444529331b3b6551d0e95fbf293a9
kyra-smith <73694089+kyra-smith@users.noreply.github.com>
2020-11-09 10:45:49 +0100

    Update R_code_plot_ecol_var.r

--- R_code_plot_ecol_var.r
+++ R_code_plot_ecol_var.r
@@ -1,3 +1,4 @@
+#### Friday, November 6, 2020 ####
 # comment
 
 # R code for plotting the relationship amongn ecological variables
@@ -42,3 +43,49 @@ plot(cadmium, lead)
 
 # want to make scatter plot matrix
 pairs(meuse)
+
+#### Monday, November 9, 2020 ####
+
+# 2020-11-09
+# lecture #2 ecological variables
+
+# call library for sp
+library(sp)
+
+# load dataset
+data(meuse)
+
+# see data
+meuse
+
+# look at the first 6 rows
+head(meuse)
+
+# how to make a scatterplot with just the metals
+# need just columns 3,4,5,6
+# use a quadratic bracket to include the four columns and all the rows)
+# use a comma to introduce new argument, so the above line says (start from 3, go to 6)
+
+pairs(meuse[,3:6])
+
+# Q: How does R know we are talking about columns and not rows? A: columns are the default; we would have to tell it more in order to isolate rows
+# Q: does R start counting from 0 or 1? A: 1
+
+# another method
+pairs(~cadmium + copper + lead + zinc, data=meuse)
+
+# "~" means "equal"
+# so the pairs will = what follows the tilde
+
+# time to prettify the graph
+# change the colour
+ pairs(~cadmium + copper + lead + zinc, data=meuse, col="sienna1")
+ 
+# we can also change the colours of the individual graphs using the par() function
+
+# now change the symbols to a filled triangle
+pairs(~cadmium + copper + lead + zinc, data=meuse, col="sienna1", pch=17)
+# no increase size of triangles
+pairs(~cadmium + copper + lead + zinc, data=meuse, col="sienna1", pch=17, cex=2.5)
+
+
